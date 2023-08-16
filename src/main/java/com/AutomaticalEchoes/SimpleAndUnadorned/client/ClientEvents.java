7a820914ce5c69eeb4cada491db7473d93db2105
1121665965 <1121665965@qq.com>
2023-08-16 13:23:14 +0800

    建立

--- src/main/java/com/AutomaticalEchoes/SimpleAndUnadorned/client/ClientEvents.java
+++ src/main/java/com/AutomaticalEchoes/SimpleAndUnadorned/client/ClientEvents.java
@@ -1,6 +1,7 @@
 package com.AutomaticalEchoes.SimpleAndUnadorned.client;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
